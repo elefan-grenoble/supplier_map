48c574c15a1a65553834dfcedc2fc7c55ad5f360
NicolasGrosjean <grosjen@hotmail.fr>
2019-09-28 17:33:27 +0200

    Init commit
    - Read a CSV file
    - Geocode all the adresses
    - Export a CSV file with name and coordinates only

---
+++ source/address_to_coords.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+import time
+from geopy.geocoders import Nominatim, BANFrance
+
+
+def get_data_dir():
+    if os.path.exists('data'):
+        return 'data'
+    if os.path.exists(os.path.join('..', 'data')):
+        return os.path.join('..', 'data')
+    assert(False)
+
+
+class AddressToCoords:
+    def __init__(self, database_file=os.path.join(get_data_dir(), 'address_to_coords.csv')):
+        self.nominatim = Nominatim()
+        self.ban = BANFrance()
+        if os.path.exists(database_file):
+            self.database_file = database_file
+            self.database = pd.read_csv(self.database_file, encoding='iso-8859-1', sep=';')
+        else:
+            self.database = pd.DataFrame()
+            self.database_file = database_file if database_file is not None else 'address_to_coords.csv'
+
+    def get_online_coordinates(self, address):
+        time.sleep(1.1) # Respect the API condition
+        location = self.ban.geocode(address)
+        if location is None:
+            location = self.nominatim.geocode(address)
+            if location is None:
+                print(f'Adress not found for : {address}')
+                return None, None
+        self.database = pd.concat([self.database, pd.DataFrame(data=[(address, location.latitude, location.longitude)],
+                                                               columns=['Address', 'lat', 'lon'])], axis=0)
+        return location.latitude, location.longitude
+
+    def save_database(self):
+        self.database.to_csv(self.database_file, index=None, sep=';')
+
+    def get_coordinates(self, address):
+        if 'Address' in self.database and address in self.database['Address'].values:
+            idx = self.database[self.database['Address'] == address].index
+            return self.database.loc[idx, 'lat'].values[0], self.database.loc[idx, 'lon'].values[0]
+        return self.get_online_coordinates(address)
+
+
+if __name__ == '__main__':
+    addr2c = AddressToCoords()
+    print(addr2c.get_coordinates("175 5th Avenue NYC"))
+    addr2c.save_database()
---
+++ source/manage_supplier_data.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+from source.address_to_coords import AddressToCoords, get_data_dir
+
+# Read data
+data = pd.read_csv(os.path.join(get_data_dir(), 'supplier_table.csv'), encoding='iso-8859-1', sep=';')
+
+# Add coordinates
+addr2c = AddressToCoords()
+data['lat'] = None
+data['lon'] = None
+try:
+    for index, row in tqdm(data.iterrows(), total=len(data)):
+        if not pd.isnull(row['Adresse 1']):
+            location = addr2c.get_coordinates(row['Adresse 1'] + ' ' + str(int(row['C.Postal'])) + ' ' + row['Ville'])
+            data.loc[index, 'lat'] = location[0]
+            data.loc[index, 'lon'] = location[1]
+finally:
+    addr2c.save_database()
+
+# Export some data
+data[['RaisonSociale', 'lat', 'lon']].to_csv(os.path.join(get_data_dir(), 'umap.csv'), index=None, sep=';')
+data2 = data[~pd.isnull(data['lat'])]
+data2.to_csv(os.path.join(get_data_dir(), 'umap_filtered.csv'), index=None, sep=';')
