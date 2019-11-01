83af801a44d70ea79efbbfd6086b4850927ace99
NicolasGrosjean <grosjen@hotmail.fr>
2019-11-01 17:55:48 +0100

    Add slogan and date
    Add data from https://lelefan.org/nos-producteurs/

--- source/main/manage_supplier_data.py
+++ source/main/manage_supplier_data.py
@@ -1,4 +1,5 @@
 import argparse
+import locale
 import os
 import pandas as pd
 from tqdm import tqdm
@@ -31,12 +32,16 @@ if __name__ == '__main__':
     finally:
         addr2c.save_database()
 
+    # Format Date
+    locale.setlocale(locale.LC_TIME, '')
+    data['DatePartenariat'] = pd.to_datetime(data['Date partenariat'], format='%d/%m/%Y').dt.strftime('%B %Y')
+    data['DatePartenariat'] = data['DatePartenariat'].apply(lambda d: '' if d == 'NaT' else d[0].upper() + d[1:])
+
     # Export some data
-    data[['RaisonSociale', 'lat', 'lon']].to_csv(os.path.join(get_data_dir(), 'umap.csv'), encoding='utf8',
-                                                 index=None, sep=';')
     missing_coordinates_nb = pd.isnull(data['lat']).sum()
     print(f'{missing_coordinates_nb} suppliers with no coordinates')
     data2 = data[~pd.isnull(data['lat'])]
-    columns_to_keep = ['RaisonSociale', 'lat', 'lon', 'Site', 'Rayon', 'Description', 'Photo', 'Produits']
+    columns_to_keep = ['RaisonSociale', 'lat', 'lon', 'Site', 'Rayon', 'Description', 'Slogan', 'Photo', 'Produits',
+                       'DatePartenariat']
     data2[columns_to_keep].to_csv(os.path.join(get_data_dir(), 'umap_filtered.csv'),
                                   encoding='utf8', index=None, sep=';')
